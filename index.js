568c77f123ab380cc921a39716718b657d941706
unknown <bilalelbedoui93@gmail.com>
2019-10-20 18:59:45 +0200

    structuring the system to start modeling

---
+++ index.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const routes = require('./routes')
+
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+
+const { env: { MONGO_URL, PORT } } = process;
+
+(async () => {
+    try {
+        await mongoose.connect(MONGO_URL || "mongodb://localhost/immfly_project", {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useFindAndModify: false,
+        });
+
+        app.use(bodyParser.json());
+        
+        app.use(cors())
+
+        app.use('/api', routes)
+
+        console.log(`connected to database MongoDB! Port:${MONGO_URL}`);
+
+        app.listen(PORT, () => console.log(`listening to the port ${PORT}...`))
+
+    } catch (error) {
+        console.log('Cannot connect to the db', error)
+    }
+})()
